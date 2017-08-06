fb23031e21bbd76c64ed46d2b9d22dde6399c3c9
Abhinay Reddy <xprmnts@protonmail.com>
2017-08-06 16:46:19 -0400

    removed bitwise operator mistake in leap.py

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,6 @@
 def is_leap_year(year):
     # if mod of 100 is 0 & mod 400 does not equal 0 return false
     # return false if mod 4 is not 0, else return true
-    if((year % 100 == 0) & (year % 400 != 0) | (year % 4 != 0)):
+    if((year % 100 == 0) and (year % 400 != 0) or (year % 4 != 0)):
         return False
     return True
